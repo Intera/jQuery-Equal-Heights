c23feea9fb1c7e6d6dbf990fbf9e4eeed43687b5
Alexander Stehlik <alexander.stehlik@gmail.com>
2017-09-14 14:58:36 +0200

    [BUGFIX] Use outerHeight() to detect element height
    This makes the plugin work with box-sizing: border-box;

--- jQuery.equalHeights.js
+++ jQuery.equalHeights.js
@@ -21,7 +21,7 @@ $.fn.equalHeights = function(px) {
 	$(this).each(function(){
 		var currentTallest = 0;
 		$(this).children().each(function(i){
-			if ($(this).height() > currentTallest) { currentTallest = $(this).height(); }
+			if ($(this).outerHeight() > currentTallest) { currentTallest = $(this).outerHeight(); }
 		});
     if (!px && Number.prototype.pxToEm) currentTallest = currentTallest.pxToEm(); //use ems unless px is specified
 		// for ie6, set height since min-height isn't supported
